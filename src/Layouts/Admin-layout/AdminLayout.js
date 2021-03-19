ac586f7e61e056355a8f9b2733144f93dde178df
Mahdiyeh <mahdiyeh.fani@gmail.com>
2021-03-19 21:55:45 +0330

    add admin panel

---
+++ src/Layouts/Admin-layout/AdminLayout.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Route, Switch } from "react-router";
+import "../../assets/css/demo.css";
+import "../../assets/css/paper-dashboard.css";
+import Login from "../../components/Login";
+import Sidebar from "./Sidebar";
+import TopNavbar from "./TopNavbar";
+
+const AdminLayout = () => {
+  return (
+    <>
+      <div class="wrapper">
+        <Sidebar />
+        <div class="main-panel">
+          <TopNavbar />
+          <div className="content">
+            <Switch>
+              <Route path="/" exact component={Login} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminLayout;
